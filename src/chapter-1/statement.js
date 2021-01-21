1bb4e1162c45ade08d0543b495ac9418b126383b
Dmitriy Davydov <dvdvdmt@gmail.com>
2021-01-21 18:40:12 +0500

    Final cleanup

--- src/chapter-1/statement.js
+++ src/chapter-1/statement.js
@@ -43,14 +43,11 @@ function usd(number) {
 
 export function statement(invoice, plays) {
   let result = `Statement for ${invoice.customer}\n`;
-
   for (let perf of invoice.performances) {
-    // print line for this order
     result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${
       perf.audience
     } seats)\n`;
   }
-
   result += `Amount owed is ${usd(totalAmount())}\n`;
   result += `You earned ${volumeCredits()} credits\n`;
   return result;
