61e160ffa7f6a8da087b004bd212e01546b94a56
Dmitriy Davydov <dvdvdmt@gmail.com>
2021-01-21 18:20:12 +0500

    Move conversion from cents to dollars to formatting function

--- src/chapter-1/statement.js
+++ src/chapter-1/statement.js
@@ -38,7 +38,7 @@ function usd(number) {
     style: 'currency',
     currency: 'USD',
     minimumFractionDigits: 2,
-  }).format(number);
+  }).format(number / 100);
 }
 
 export function statement(invoice, plays) {
@@ -49,12 +49,12 @@ export function statement(invoice, plays) {
   for (let perf of invoice.performances) {
     volumeCredits += volumeCreditsFor(perf);
     // print line for this order
-    result += ` ${playFor(perf).name}: ${usd(amountFor(perf) / 100)} (${
+    result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${
       perf.audience
     } seats)\n`;
     totalAmount += amountFor(perf);
   }
-  result += `Amount owed is ${usd(totalAmount / 100)}\n`;
+  result += `Amount owed is ${usd(totalAmount)}\n`;
   result += `You earned ${volumeCredits} credits\n`;
   return result;
 
