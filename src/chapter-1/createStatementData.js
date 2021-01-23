3e61cdbb1cf28f17c2df35a32cdb00fcce789ba9
Dmitriy Davydov <dvdvdmt@gmail.com>
2021-01-24 00:55:38 +0500

    Move calculations to class

--- src/chapter-1/createStatementData.js
+++ src/chapter-1/createStatementData.js
@@ -4,6 +4,10 @@ class PerformanceCalculator {
     this.play = play;
   }
 
+  get audience() {
+    return this.performance.audience;
+  }
+
   get amount() {
     let result = 0;
     switch (this.play.type) {
@@ -44,29 +48,7 @@ export function createStatementData(invoice, plays) {
   return result;
 
   function totalAmount(data) {
-    return data.performances.reduce((total, p) => total + amountFor(p), 0);
-  }
-
-  function amountFor(performance) {
-    let result = 0;
-    switch (performance.play.type) {
-      case 'tragedy':
-        result = 40000;
-        if (performance.audience > 30) {
-          result += 1000 * (performance.audience - 30);
-        }
-        break;
-      case 'comedy':
-        result = 30000;
-        if (performance.audience > 20) {
-          result += 10000 + 500 * (performance.audience - 20);
-        }
-        result += 300 * performance.audience;
-        break;
-      default:
-        throw new Error(`unknown type: ${performance.play.type}`);
-    }
-    return result;
+    return data.performances.reduce((total, p) => total + p.amount, 0);
   }
 
   function playFor(perf) {
@@ -78,14 +60,6 @@ export function createStatementData(invoice, plays) {
   }
 
   function enrichPerformance(performance) {
-    const calculator = new PerformanceCalculator(
-      performance,
-      playFor(performance)
-    );
-    let result = {...performance};
-    result.play = calculator.play;
-    result.amount = calculator.amount;
-    result.volumeCredits = calculator.volumeCredits;
-    return result;
+    return new PerformanceCalculator({...performance}, playFor(performance));
   }
 }
