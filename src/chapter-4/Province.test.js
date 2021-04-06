7d92123d7f11dfb1db49f5f3addc6406ad47f3ad
Dmitriy Davydov <dvdvdmt@gmail.com>
2021-04-06 08:38:00 +0500

    Add test that checks calculation of shortfall and profit when production changes

--- src/chapter-4/Province.test.js
+++ src/chapter-4/Province.test.js
@@ -15,4 +15,10 @@ describe(`Province`, () => {
   it(`profit`, () => {
     expect(asia.profit).toBe(230);
   });
+
+  it(`changes production`, () => {
+    asia.producers[0].production = 20;
+    expect(asia.shortfall).toBe(-6);
+    expect(asia.profit).toBe(292);
+  });
 });
