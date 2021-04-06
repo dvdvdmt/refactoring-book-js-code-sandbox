423adb852a2a5a4788755267623bf089aa266e48
Dmitriy Davydov <dvdvdmt@gmail.com>
2021-04-06 09:41:21 +0500

    Add boundary tests

--- src/chapter-4/Province.test.js
+++ src/chapter-4/Province.test.js
@@ -16,9 +16,49 @@ describe(`Province`, () => {
     expect(asia.profit).toBe(230);
   });
 
-  it(`changes production`, () => {
+  it(`change production`, () => {
     asia.producers[0].production = 20;
     expect(asia.shortfall).toBe(-6);
     expect(asia.profit).toBe(292);
+  });
+
+  it(`zero demand`, () => {
+    asia.demand = 0;
+    expect(asia.shortfall).toBe(-25);
+    expect(asia.profit).toBe(0);
+  });
+
+  it(`negative demand`, () => {
+    asia.demand = -1;
+    expect(asia.shortfall).toBe(-26);
+    expect(asia.profit).toBe(-10);
+  });
+
+  it(`empty string demand`, () => {
+    asia.demand = '';
+    expect(asia.shortfall).toBeNaN();
+    expect(asia.profit).toBeNaN();
+  });
+
+  describe(`no producers`, () => {
+    let noProducers;
+
+    beforeEach(function () {
+      const data = {
+        name: 'No producers',
+        producers: [],
+        demand: 30,
+        price: 20,
+      };
+      noProducers = new Province(data);
+    });
+
+    it('shortfall', function () {
+      expect(noProducers.shortfall).toBe(30);
+    });
+
+    it('profit', function () {
+      expect(noProducers.profit).toBe(0);
+    });
   });
 });
